d97877f37d63273b559c3b205ebc49f435c53c35
Omar Sanchez <osanchez@umass.edu>
2018-03-29 16:52:44 -0400

    final commit

--- istat_fat16.py
+++ istat_fat16.py
@@ -8,7 +8,7 @@ def as_unsigned(bs, endian='<'):
         raise ValueError()
     fill = '\x00'
     while len(bs) not in unsigned_format:
-        bs = bs + fill
+        bs += fill
     result = struct.unpack(endian + unsigned_format[len(bs)], bs)[0]
     return result
 
@@ -31,15 +31,142 @@ def decode_fat_day(date_bytes):
     return '{}-{:02}-{:02}'.format(year, month, day)
 
 
+def get_root_area_end(b, root_area_start, bytes_per_sector):
+    return root_area_start + (as_unsigned(b[17:19]) * 32 // bytes_per_sector) - 1
+
+
 def istat_fat16(f, address, sector_size=512, offset=0):
-    pass
+    result = []
+    f.seek(offset * sector_size)
+    boot_sector = f.read(sector_size)
+    fat_size = fsstat_fat16.get_fat_size(boot_sector)  # reuse old code
+    sectors_before_start = fsstat_fat16.get_sectors_start(boot_sector)  # reuse old code
+    fat_start = sectors_before_start + 1
+    number_of_fats = fsstat_fat16.get_number_fats(boot_sector)
+    reserved_area_size = fsstat_fat16.get_reserved_area_size(boot_sector)
+    root_area_start = sectors_before_start - offset + reserved_area_size + number_of_fats * fat_size
+    bytes_per_sector = fsstat_fat16.get_bytes_per_sector(boot_sector)
+    root_area_end = get_root_area_end(boot_sector, root_area_start, bytes_per_sector)
+    cluster_start = root_area_end + 1
+    cluster_size = fsstat_fat16.get_cluster_size(boot_sector)
+    f.seek(offset * sector_size + ((root_area_start * sector_size) + (address - 3) * 32))
+    direc_entry = f.read(32)
+
+    result.append('Directory Entry: ' + str(address))
+
+    if direc_entry[0] == 0xe5:
+        result.append('Not Allocated')
+    if direc_entry[0] == 0x00:
+        result.append('Not Allocated')
+    else:
+        result.append('Allocated')
+
+    # TODO: refine - look into flag identities
+    attribute = direc_entry[11]
+    file_attribute = "File Attributes: "
+    if attribute & 0x0f == 0x0f:
+        file_attribute += 'Long File Name'
+    else:
+        if attribute & 0x10:
+            file_attribute += "Directory"
+        elif attribute & 0x08:
+            file_attribute += "Volume Label"
+        else:
+            file_attribute += "File"
+        if attribute & 0x01:
+            file_attribute += ", Read Only"
+        if attribute & 0x02:
+            file_attribute += ", Hidden"
+        if attribute & 0x04:
+            file_attribute += ", System"
+        if attribute & 0x20:
+            file_attribute += ", Archive"
+    result.append(file_attribute)
+
+    file_size = as_unsigned(direc_entry[28:32])
+    f.seek(fat_start * sector_size)
+    fat = f.read(fat_size * sector_size)
+    fat = fat[4:]
+    count = 0
+    sector_count = file_size // sector_size
+    cluster_number = fsstat_fat16.get_cluster_to_sector(as_unsigned(direc_entry[26:28]), cluster_size)
+    cluster_offset = as_unsigned(fat[cluster_number:cluster_number + 2])
+    cluster_line = []
+    cluster_result = []
+    flag = False
+
+    while cluster_number < len(fat) and sector_count > 0:
+        flag = True
+        if len(cluster_line) == 8:
+            cluster_result.append(" ".join(cluster_line))
+            cluster_line = []
+        for c in range(cluster_size):
+            cluster_line.append(str(cluster_start + cluster_number + c))
+        if 0xffff > cluster_offset > 0:
+            cluster_number = fsstat_fat16.get_cluster_to_sector(cluster_offset, cluster_size)
+        elif cluster_offset == 0xffff:
+            flag = True
+            break
+        else:
+            cluster_number += 2
+        cluster_offset = as_unsigned(fat[cluster_number:cluster_number + 2])
+        sector_count -= 2
+        count += 2
+    if len(cluster_line) == 8:
+        cluster_result.append(" ".join(cluster_line))
+        cluster_line = []
+
+    if file_size == 0:
+        for c in range(cluster_size):
+            cluster_line.append(str(cluster_start + cluster_number + c))
+            count += 1
+    elif not flag or direc_entry[0] == 0xe5:
+        rem_size = file_size % (sector_size * cluster_size)
+        num_zeroes = rem_size // sector_size + 1
+        for c in range(cluster_size - num_zeroes):
+            cluster_line.append(str(cluster_start + cluster_number + c))
+            count += 1
+        for c in range(num_zeroes):
+            cluster_line.append("0")
+            count += 1
+    if file_size == 0:
+        result.append('Size: ' + str(count * sector_size))
+    else:
+        result.append('Size: ' + str(file_size))
+
+     # TODO: check
+
+    file_ext = "".join(i for i in direc_entry[8:12].decode('ascii') if 48 < ord(i) < 127)
+    lowercase_byte = direc_entry[12]
+    if direc_entry[0] == 0xe5:
+        filename = '_'
+    else:
+        filename = direc_entry[0:1].decode('ascii').strip()
+    filename += direc_entry[1:8].decode('ascii').strip()
+    if lowercase_byte & 0x08:
+        filename = filename.lower()
+    if file_ext:
+        if lowercase_byte & 0x10:
+            filename += "." + file_ext.lower()
+        else:
+            filename += "." + file_ext
+    result.append('Name: ' + filename)
+
+    result.append('')
+    result.append('Directory Entry Times:')
+    result.append('Written:\t' + decode_fat_day(direc_entry[24:26]) + " " + decode_fat_time(direc_entry[22:24]))
+    result.append('Accessed:\t' + decode_fat_day(direc_entry[18:20]) + " " + decode_fat_time(bytes.fromhex('0000')))
+    result.append(
+        'Created:\t' + decode_fat_day(direc_entry[16:18]) + " " + decode_fat_time(direc_entry[14:16], direc_entry[13]))
+    result.append('')
+
+    result.append('Sectors:')
+    for c in cluster_result:
+        result.append(c)
+    if len(cluster_line) > 0:
+        result.append(" ".join(cluster_line))
+    return result
 
 
 if __name__ == '__main__':
-    # The code below just exercises the time/date decoder and need not be included
-    # in your final submission!
-    #
-    # values below are from the directory entry in adams.dd that corresponds to the
-    # creation date/time of the `IMAGES` directory in the root directory, at
-    # metadata address 5; it starts at offset 0x5240 from the start of the image
-    print(decode_fat_day(bytes.fromhex('E138')), decode_fat_time(bytes.fromhex('C479'), 0))
+    pass
